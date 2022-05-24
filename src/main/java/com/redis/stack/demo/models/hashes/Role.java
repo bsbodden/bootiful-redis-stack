b25919b77e25ffbaf1090b723ee5da257e33370e
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2022-05-24 10:34:42 +0200

    step 6: add SDR @RedisHash to Role POJO

--- src/main/java/com/redis/stack/demo/models/hashes/Role.java
+++ src/main/java/com/redis/stack/demo/models/hashes/Role.java
@@ -1,10 +1,13 @@
 package com.redis.stack.demo.models.hashes;
 
+import org.springframework.data.redis.core.RedisHash;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@RedisHash
 public class Role {
   private String id;
   private String name;
