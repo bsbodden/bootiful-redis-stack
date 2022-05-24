4c60ce2d61f0d62bd8226306fe50fc91b37a3049
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2022-05-24 19:30:47 +0200

    step 45: add RepositoryDrivenController

---
+++ src/main/java/com/redis/stack/demo/controllers/RepositoryDrivenController.java
@@ -0,0 +1,76 @@
+package com.redis.stack.demo.controllers;
+
+import java.util.Optional;
+import java.util.Set;
+
+import com.redis.stack.demo.models.json.FictionalCharacter;
+import com.redis.stack.demo.repositories.json.FictionalCharacterRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.domain.geo.Metrics;
+
+@RestController
+@RequestMapping("/api/rdc/")
+public class RepositoryDrivenController {
+  @Autowired
+  FictionalCharacterRepository repo;
+
+  @GetMapping("all")
+  Iterable<FictionalCharacter> all() {
+    return repo.findAll();
+  }
+
+  @GetMapping("{id}")
+  Optional<FictionalCharacter> byId(@PathVariable String id) {
+    return repo.findById(id);
+  }
+
+  @GetMapping("age_between")
+  Iterable<FictionalCharacter> byAgeBetween( //
+      @RequestParam("min") int min, //
+      @RequestParam("max") int max) {
+    return repo.findByActorAgeBetween(min, max);
+  }
+
+  @GetMapping("name")
+  Iterable<FictionalCharacter> byFirstNameAndLastName(@RequestParam("first") String firstName, //
+      @RequestParam("last") String lastName) {
+    return repo.findByActorFirstNameAndActorLastName(firstName, lastName);
+  }
+
+  @GetMapping("homeloc")
+  Iterable<FictionalCharacter> byHomeLoc(//
+      @RequestParam("lat") double lat, //
+      @RequestParam("lon") double lon, //
+      @RequestParam("d") double distance) {
+    return repo.findByActorLocationNear(new Point(lon, lat), new Distance(distance, Metrics.MILES));
+  }
+
+  @GetMapping("statement")
+  Iterable<FictionalCharacter> byPersonalStatement(@RequestParam("q") String q) {
+    return repo.searchByQuote(q);
+  }
+
+  @GetMapping("city")
+  Iterable<FictionalCharacter> byCity(@RequestParam("city") String city) {
+    return repo.findByActorAddress_City(city);
+  }
+
+  @GetMapping("skills")
+  Iterable<FictionalCharacter> byAnySkills(@RequestParam("skills") Set<String> skills) {
+    return repo.findBySkills(skills);
+  }
+
+  @GetMapping("skills/all")
+  Iterable<FictionalCharacter> byAllSkills(@RequestParam("skills") Set<String> skills) {
+    return repo.findBySkillsContainingAll(skills);
+  }
+}
