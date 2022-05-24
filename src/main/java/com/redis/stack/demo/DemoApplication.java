49869ce5e999eaace3963201422f200cb9802168
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2022-05-24 11:08:06 +0200

    step 9: add @EnableRedisEnhancedRepositories

--- src/main/java/com/redis/stack/demo/DemoApplication.java
+++ src/main/java/com/redis/stack/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.redis.stack.demo;
 
+import com.redis.om.spring.annotations.EnableRedisEnhancedRepositories;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableRedisEnhancedRepositories
 public class DemoApplication {
 
   public static void main(String[] args) {
