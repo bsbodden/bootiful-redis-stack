eb0a4ef203857aa9af7127f10a1adf5a7bd1d86c
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2022-05-24 19:28:52 +0200

    step 44: add FictionalCharacters sample data

--- src/main/java/com/redis/stack/demo/DemoApplication.java
+++ src/main/java/com/redis/stack/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
+import java.util.Set;
 
 import com.google.common.collect.Lists;
 import com.google.gson.Gson;
@@ -14,8 +15,11 @@ import com.redis.om.spring.annotations.EnableRedisDocumentRepositories;
 import com.redis.om.spring.annotations.EnableRedisEnhancedRepositories;
 import com.redis.stack.demo.models.hashes.Role;
 import com.redis.stack.demo.models.hashes.User;
+import com.redis.stack.demo.models.json.Address;
+import com.redis.stack.demo.models.json.FictionalCharacter;
 import com.redis.stack.demo.repositories.hashes.RoleRepository;
 import com.redis.stack.demo.repositories.hashes.UserRepository;
+import com.redis.stack.demo.repositories.json.FictionalCharacterRepository;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -23,6 +27,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.geo.Point;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -39,7 +44,7 @@ public class DemoApplication {
   private static final Log logger = LogFactory.getLog(DemoApplication.class);
 
   @Bean
-  CommandLineRunner loadTestData(RoleRepository roleRepository, UserRepository userRepository) {
+  CommandLineRunner loadTestData(RoleRepository roleRepository, UserRepository userRepository, FictionalCharacterRepository fcRepository) {
     logger.info("🚀 Loading test data...");
     return args -> {
       // Load Roles
@@ -76,6 +81,46 @@ public class DemoApplication {
         }
       } else {
         logger.info(String.format("✅ There are %s Users...", userRepository.count()));
+      }
+
+      // Load FictionalCharacters
+      if (fcRepository.count() == 0) {
+        String thorSays = "The Rabbit Is Correct, And Clearly The Smartest One Among You.";
+        String ironmanSays = "Doth mother know you weareth her drapes?";
+        String blackWidowSays = "Hey, fellas. Either one of you know where the Smithsonian is? I'm here to pick up a fossil.";
+        String wandaMaximoffSays = "You Guys Know I Can Move Things With My Mind, Right?";
+        String gamoraSays = "I Am Going To Die Surrounded By The Biggest Idiots In The Galaxy.";
+        String nickFurySays = "Sir, I'm Gonna Have To Ask You To Exit The Donut";
+
+        // Serendipity, 248 Seven Mile Beach Rd, Broken Head NSW 2481, Australia
+        Address thorsAddress = Address.of("248", "Seven Mile Beach Rd", "Broken Head", "NSW", "2481", "Australia");
+
+        // 11 Commerce Dr, Riverhead, NY 11901
+        Address ironmansAddress = Address.of("11", "Commerce Dr", "Riverhead", "NY",  "11901", "US");
+
+        // 605 W 48th St, New York, NY 10019
+        Address blackWidowAddress = Address.of("605", "48th St", "New York", "NY", "10019", "US");
+
+        // 20 W 34th St, New York, NY 10001
+        Address wandaMaximoffsAddress = Address.of("20", "W 34th St", "New York", "NY", "10001", "US");
+
+        // 107 S Beverly Glen Blvd, Los Angeles, CA 90024
+        Address gamorasAddress = Address.of("107", "S Beverly Glen Blvd", "Los Angeles", "CA", "90024", "US");
+
+        // 11461 Sunset Blvd, Los Angeles, CA 90049
+        Address nickFuryAddress = Address.of("11461", "Sunset Blvd", "Los Angeles", "CA", "90049", "US");
+
+        FictionalCharacter thor = FictionalCharacter.of("Chris", "Hemsworth", 38, thorSays, new Point(153.616667, -28.716667), thorsAddress, Set.of("hammer", "biceps", "hair", "heart"));
+        FictionalCharacter ironman = FictionalCharacter.of("Robert", "Downey", 56, ironmanSays, new Point(40.9190747, -72.5371874), ironmansAddress, Set.of("tech", "money", "one-liners", "intelligence", "resources"));
+        FictionalCharacter blackWidow = FictionalCharacter.of("Scarlett", "Johansson", 37, blackWidowSays, new Point(40.7215259, -74.0129994), blackWidowAddress, Set.of("deception", "martial_arts"));
+        FictionalCharacter wandaMaximoff = FictionalCharacter.of("Elizabeth", "Olsen", 32, wandaMaximoffSays, new Point(40.6976701, -74.2598641), wandaMaximoffsAddress, Set.of("magic", "loyalty"));
+        FictionalCharacter gamora = FictionalCharacter.of("Zoe", "Saldana", 43, gamoraSays, new Point(-118.399968, 34.073087), gamorasAddress, Set.of("skills", "martial_arts"));
+        FictionalCharacter nickFury = FictionalCharacter.of("Samuel L.", "Jackson", 73, nickFurySays, new Point(-118.4345534, 34.082615), nickFuryAddress, Set.of("planning", "deception", "resources"));
+
+        fcRepository.saveAll(List.of(thor, ironman, blackWidow, wandaMaximoff, gamora, nickFury));
+        logger.info(String.format("✅ Created %s Fictional Characters...", fcRepository.count()));
+      } else {
+        logger.info(String.format("✅ There are %s Fictional Characters...", fcRepository.count()));
       }
     };
   }
