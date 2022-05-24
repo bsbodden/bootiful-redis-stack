051c29064c2d93df8e5b32dd63e99832ab47f3e4
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2022-05-24 19:04:41 +0200

    step 37: add FictionalCharacterRepository#findByActorAgeBetween

--- src/main/java/com/redis/stack/demo/repositories/json/FictionalCharacterRepository.java
+++ src/main/java/com/redis/stack/demo/repositories/json/FictionalCharacterRepository.java
@@ -4,4 +4,6 @@ import com.redis.om.spring.repository.RedisDocumentRepository;
 import com.redis.stack.demo.models.json.FictionalCharacter;
 
 public interface FictionalCharacterRepository extends RedisDocumentRepository<FictionalCharacter, String> {
+  // Find people by age range
+  Iterable<FictionalCharacter> findByActorAgeBetween(int minAge, int maxAge);
 }
