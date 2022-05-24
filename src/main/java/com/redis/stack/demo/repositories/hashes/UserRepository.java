a0c4943cafd97039171b5c539a0c8d3a9392f98b
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2022-05-24 15:15:36 +0200

    step 32: add findFirstByEmail finder to UserRepository

--- src/main/java/com/redis/stack/demo/repositories/hashes/UserRepository.java
+++ src/main/java/com/redis/stack/demo/repositories/hashes/UserRepository.java
@@ -1,5 +1,7 @@
 package com.redis.stack.demo.repositories.hashes;
 
+import java.util.Optional;
+
 import com.redis.stack.demo.models.hashes.User;
 
 import org.springframework.data.repository.CrudRepository;
@@ -8,4 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
   Iterable<User> findByNameStartingWith(String prefix);
+
+  Optional<User> findFirstByEmail(String email);
 }
