7c85d56480aab0d6eac32388e16c46235313d53a
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2022-05-24 19:07:12 +0200

    step 39: add FictionalCharacterRepository#findByActorLocationNear

--- src/main/java/com/redis/stack/demo/repositories/json/FictionalCharacterRepository.java
+++ src/main/java/com/redis/stack/demo/repositories/json/FictionalCharacterRepository.java
@@ -3,10 +3,17 @@ package com.redis.stack.demo.repositories.json;
 import com.redis.om.spring.repository.RedisDocumentRepository;
 import com.redis.stack.demo.models.json.FictionalCharacter;
 
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Point;
+
 public interface FictionalCharacterRepository extends RedisDocumentRepository<FictionalCharacter, String> {
   // Find people by age range
   Iterable<FictionalCharacter> findByActorAgeBetween(int minAge, int maxAge);
 
   // Find people by their first and last name
   Iterable<FictionalCharacter> findByActorFirstNameAndActorLastName(String firstName, String lastName);
+
+  // Draws a circular geofilter around a spot and returns all people in that
+  // radius
+  Iterable<FictionalCharacter> findByActorLocationNear(Point point, Distance distance);
 }
