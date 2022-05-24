626f12b15ed73c577a7b3637f686efd5fef29e97
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2022-05-24 13:36:45 +0200

    step 29: add findByNameStartingWith finder to UserRepository

--- src/main/java/com/redis/stack/demo/repositories/hashes/UserRepository.java
+++ src/main/java/com/redis/stack/demo/repositories/hashes/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
+  Iterable<User> findByNameStartingWith(String prefix);
 }
