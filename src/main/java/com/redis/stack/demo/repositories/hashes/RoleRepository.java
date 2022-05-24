5a61f6dab87cc5bde2b3d62414a0a35f3f0bd0ad
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2022-05-24 12:42:28 +0200

    step 20: add findFirstByName finder to RoleRepository

--- src/main/java/com/redis/stack/demo/repositories/hashes/RoleRepository.java
+++ src/main/java/com/redis/stack/demo/repositories/hashes/RoleRepository.java
@@ -1,5 +1,7 @@
 package com.redis.stack.demo.repositories.hashes;
 
+import java.util.Optional;
+
 import com.redis.stack.demo.models.hashes.Role;
 
 import org.springframework.data.repository.CrudRepository;
@@ -7,4 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends CrudRepository<Role, String> {
+  Optional<Role> findFirstByName(String name);
 }
